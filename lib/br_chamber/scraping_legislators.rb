c0ab9a34e7f0359342841b857144650e8adcd7ac
Helder Ribeiro <helder@gmail.com>
2009-10-30 13:00:56 -0200

    Extract option parsing and helper into a separate file.

--- lib/br_chamber/scraping_legislators.rb
+++ lib/br_chamber/scraping_legislators.rb
@@ -3,62 +3,9 @@ require 'rubygems'
 require 'fileutils'
 require 'iconv'
 require 'fastercsv'
-require 'optparse'
+require File.expand_path(File.join(File.dirname( __FILE__),
+                                   'scraper_common'))
 
-module ScrapingHelper
-
-  # Expects a full URL, beginning with 'http://'
-  def url_path(url)
-    path = url[7..-1] # remove 'http://'
-    path.split('/')[0...-1].join('/')
-  end
-
-end
-
-class ScraperOptions
-
-  def self.parse(args)
-    script_name = File.basename($0)
-    options = {}
-
-    opts = OptionParser.new do |opts|
-      opts.banner = "Usage: #{script_name}"
-
-      opts.on(:REQUIRED, '-dDIR', '--data=DIR', 'Output directory for ' +
-              'parsed data (optional, defaults to ./data)') do |dir|
-        options[:data_dir] = dir
-      end
-
-      opts.on(:REQUIRED, '-sDIR', '--source-data=DIR', 'Output directory ' +
-              'for raw data (optional, defaults to ./source_data)') do |dir|
-        options[:source_data_dir] = dir
-      end
-
-      opts.on(:NONE, '--no-parse', "Only download pages to be scraped, " +
-              "but don't parse them") do |dir|
-        options[:noparse] = true
-      end
-
-      opts.on(:NONE, '--no-get',
-              'Parse pages without downloading them.') do |dir|
-        options[:noget] = true
-      end
-    end
-
-    begin
-      opts.parse!(args)
-    rescue => e
-      puts e.message.capitalize + "\n\n"
-      puts opts
-      exit 1
-    end
-
-    options[:data_dir] ||= File.join(Dir.pwd, 'data')
-    options[:source_data_dir] ||= File.join(Dir.pwd, 'source_data')
-
-    options
-  end
-end
 
 class LegislatorScraper
   include ScrapingHelper
